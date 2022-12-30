811342647f47ffbf22961a95264d68b203118fa8
saltfile <3205617363@qq.com>
2022-12-30 21:42:47 +0800

    12-30

--- Take-Out-Web/src/main/java/com/saltfish/example/dao/mealDao.java
+++ Take-Out-Web/src/main/java/com/saltfish/example/dao/mealDao.java
@@ -10,35 +10,37 @@ import java.util.List;
 @Mapper
 public interface mealDao {
     @Results({
+            @Result(property = "num", column = "num"),
             @Result(property = "type", column = "type"),
             @Result(property = "name", column = "name"),
             @Result(property = "good", column = "good"),
             @Result(property = "medim_poor", column = "medim_poor"),
             @Result(property = "sum", column = "sum"),
             @Result(property = "intro", column = "intro"),
-            @Result(property = "teste", column = "teste"),
+            @Result(property = "taste", column = "taste"),
             @Result(property = "img", column = "img")
     })
-    @Select("SELECT type,name,good,medim_poor,sum,intro,teste,img FROM meal")
+    @Select("SELECT num,type,name,good,medim_poor,sum,intro,taste,img FROM meal")
     public List<Meal> findAll();
 
     @Results({
+            @Result(property = "num", column = "num"),
             @Result(property = "type", column = "type"),
             @Result(property = "name", column = "name"),
             @Result(property = "good", column = "good"),
             @Result(property = "medim_poor", column = "medim_poor"),
             @Result(property = "sum", column = "sum"),
             @Result(property = "intro", column = "intro"),
-            @Result(property = "teste", column = "teste"),
+            @Result(property = "taste", column = "taste"),
             @Result(property = "img", column = "img")
     })
-    @Select("SELECT type,name,good,medim_poor,sum,intro,teste,img FROM meal where type = #{type}")
+    @Select("SELECT num,type,name,good,medim_poor,sum,intro,taste,img FROM meal where type = #{type}")
     public List<Meal> findByType(@Param("type")String type);
 
     @Select("SELECT * FROM meal WHERE name=#{name}")
     Meal find(@Param("name")String name);
 
-    @Insert("insert into meal(type,name,good,medim_poor,sum,intro,teste,img)values (#{type},#{name},#{good},#{medim_poor},#{sum},#{intro},#{teste},#{img})")
+    @Insert("insert into meal(type,name,good,medim_poor,sum,intro,taste,img)values (#{type},#{name},#{good},#{medim_poor},#{sum},#{intro},#{teste},#{img})")
     int insert(@Param("type")String type,@Param("name")String name,
                @Param("good")int good,@Param("medim_poor")int medim_poor,
                @Param("sum")double sum,@Param("intro")String intro,
@@ -49,8 +51,6 @@ public interface mealDao {
 
     @Update("UPDATE meal SET intro = #{intro},img=#{img} WHERE name = #{name} ")
     int Update(@Param("intro")String intro, @Param("img")String img, @Param("name") String name);
-
-
 
 
 }
